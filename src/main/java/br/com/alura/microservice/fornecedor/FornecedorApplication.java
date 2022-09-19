3afbe97c186082d849a64f5efa5217961585fafd
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-19 16:59:55 -0300

    Service Discovery Eureka

--- src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -3,7 +3,9 @@ package br.com.alura.microservice.fornecedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
