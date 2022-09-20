73c7f6be78e280d70bbdc63e4dc7619a4c2d23ad
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-20 14:09:17 -0300

    Importando pedido e produtos

--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,9 +1,11 @@
 package br.com.alura.microservice.loja.service;
 
 import br.com.alura.microservice.loja.client.FornecedorCLient;
-import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.CompraDTO;
 
-import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.model.Compra;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +15,21 @@ public class CompraService {
 
     @Autowired
     private FornecedorCLient fornecedorCLient;
-    public void realizaCompra(CompraDTO compra) {
+    public Compra realizaCompra(CompraDTO compra) {
 
         InfoFornecedorDTO info = fornecedorCLient.getInfoPorEstado(compra.getEndereco().getEstado());
+        InfoPedidoDTO pedido = fornecedorCLient.realizaPedido(compra.getItens());
 
         System.out.println(info.getEndereco());
+
+        Compra compraSalva = new Compra();
+
+        compraSalva.setPedidoId(pedido.getId());
+        compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+        compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+
+        return compraSalva;
+
 
     }
 }
