e30ef6c60c0aea9ee9efa588660144f5543996be
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-20 11:08:01 -0300

    Implementação com Spring Feign

---
+++ src/main/java/br/com/alura/microservice/loja/client/FornecedorCLient.java
@@ -0,0 +1,15 @@
+package br.com.alura.microservice.loja.client;
+
+import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("fornecedor")
+public interface FornecedorCLient {
+
+    @RequestMapping("/info/{estado}")
+    InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+
+
+}
--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,34 +1,23 @@
 package br.com.alura.microservice.loja.service;
 
+import br.com.alura.microservice.loja.client.FornecedorCLient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
 
 @Service
 public class CompraService {
 
     @Autowired
-    private RestTemplate client;
-
-    @Autowired
-    private DiscoveryClient eurekaClient;
-
+    private FornecedorCLient fornecedorCLient;
     public void realizaCompra(CompraDTO compra) {
 
-        ResponseEntity<InfoFornecedorDTO> exchange =
-                client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
-                        HttpMethod.GET, null, InfoFornecedorDTO.class);
+        InfoFornecedorDTO info = fornecedorCLient.getInfoPorEstado(compra.getEndereco().getEstado());
 
-        eurekaClient.getInstances("fornecedor").stream()
-                .forEach(fornecedor ->
-                        System.out.println("localhost: " + fornecedor.getPort()));
-
-        System.out.println(exchange.getBody().getEndereco());
+        System.out.println(info.getEndereco());
 
     }
 }
