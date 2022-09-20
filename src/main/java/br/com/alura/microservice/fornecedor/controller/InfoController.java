0249df76705af71b724e052cac5eed8fe1921076
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-20 14:37:36 -0300

    Distributed tracing

--- src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -2,6 +2,8 @@ package br.com.alura.microservice.fornecedor.controller;
 
 import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
 import br.com.alura.microservice.fornecedor.service.InfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/info")
 public class InfoController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
+
     @Autowired
     private InfoService infoService;
 
     @RequestMapping("/{estado}")
     public InfoFornecedor getInfoPorEstado(@PathVariable String estado){
 
+        LOG.info("recebido o pedido de informações do fornecedor de {}", estado);
         return infoService.getInfoPorEstado(estado);
 
     }
