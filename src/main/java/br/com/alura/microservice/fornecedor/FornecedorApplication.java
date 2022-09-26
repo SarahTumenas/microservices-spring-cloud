aaa22254f0699c1e0f66697c0a6bf637db0a4584
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-26 13:55:34 -0300

    Integração com servidor de autenticação

--- src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -2,10 +2,11 @@ package br.com.alura.microservice.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
 @SpringBootApplication
+@EnableResourceServer
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
