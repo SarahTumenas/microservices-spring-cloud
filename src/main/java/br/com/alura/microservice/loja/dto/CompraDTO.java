6c2ee5b23d382842980793057be352f31ba75052
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-26 09:02:40 -0300

    Status e situação da compra

--- src/main/java/br/com/alura/microservice/loja/dto/CompraDTO.java
+++ src/main/java/br/com/alura/microservice/loja/dto/CompraDTO.java
@@ -1,13 +1,17 @@
 package br.com.alura.microservice.loja.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.List;
 
 public class CompraDTO {
 
+    @JsonIgnore
+    private Long compraId;
+
     private List<ItemDaCompraDTO> itens;
 
     private EnderecoDTO endereco;
-
 
     public List<ItemDaCompraDTO> getItens() {
         return itens;
@@ -24,4 +28,13 @@ public class CompraDTO {
     public void setEndereco(EnderecoDTO endereco) {
         this.endereco = endereco;
     }
+
+    public Long getCompraId() {
+        return compraId;
+    }
+
+    public void setCompraId(Long compraId) {
+        this.compraId = compraId;
+    }
+
 }
