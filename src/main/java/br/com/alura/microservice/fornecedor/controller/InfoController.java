3324372ee562dea3c1448d1282ab82e83a76ecf5
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-26 08:38:53 -0300

    Atualiza controllers

--- src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -2,8 +2,7 @@ package br.com.alura.microservice.fornecedor.controller;
 
 import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
 import br.com.alura.microservice.fornecedor.service.InfoService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/info")
 public class InfoController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
-
     @Autowired
     private InfoService infoService;
 
     @RequestMapping("/{estado}")
-    public InfoFornecedor getInfoPorEstado(@PathVariable String estado){
-
-        LOG.info("recebido o pedido de informações do fornecedor de {}", estado);
+    public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
         return infoService.getInfoPorEstado(estado);
-
     }
 }
